73be4dded71438652ec6ecfece62292d8700c544
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2021-10-05 14:15:04 +0200

    Added back button to forgot password screen

--- lib/auth/screens/forgot_password.screen.dart
+++ lib/auth/screens/forgot_password.screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:fundl_app/api/models/user.model.dart';
 import 'package:fundl_app/auth/models/forgot_password.dto.dart';
+import 'package:fundl_app/common/widgets/back_button.widget.dart';
 import 'package:fundl_app/common/widgets/text_field.widget.dart';
 import 'package:fundl_app/common/widgets/text_icon_button.widgets.dart';
 
@@ -12,72 +13,84 @@ class ForgotPasswordScreen extends StatelessWidget {
 
   final _emailController = TextEditingController();
 
+  void _handleResetPassword(BuildContext context) async {
+    await User.forgotPassword(
+      ForgotPasswordDto(
+        email: _emailController.text,
+      ),
+    );
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Email sent'),
+        content: Text('The reset password link has been sent to ${_emailController.text}'),
+        actions: [
+          TextButton(
+            child: const Text('Ok'),
+            onPressed: Navigator.of(context).pop,
+          ),
+        ],
+      ),
+    );
+    Navigator.of(context).pop();
+  }
+
+  void _handleBack(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    void _handleResetPassword() async {
-      await User.forgotPassword(
-        ForgotPasswordDto(
-          email: _emailController.text,
-        ),
-      );
-      await showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('Email sent'),
-          content: Text('The reset password link has been sent to ${_emailController.text}'),
-          actions: [
-            TextButton(
-              child: const Text('Ok'),
-              onPressed: Navigator.of(context).pop,
-            ),
-          ],
-        ),
-      );
-      Navigator.of(context).pop();
-    }
-
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(40.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 100.0),
-              const Text(
-                'Reset password',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Stack(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 100.0),
+                  const Text(
+                    'Reset password',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Text(
+                    'Please enter your email address to request a password reset',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  FundlTextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Your Email',
+                      prefixIcon: Icon(IconlyLight.message),
+                    ),
+                    controller: _emailController,
+                  ),
+                  const SizedBox(height: 30.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextIconButton(
+                      text: 'RESET PASSWORD',
+                      icon: IconlyLight.arrowRight,
+                      onClick: () => _handleResetPassword(context),
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20.0),
-              const Text(
-                'Please enter your email address to request a password reset',
-                style: TextStyle(
-                  fontSize: 15.0,
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              FundlTextField(
-                decoration: const InputDecoration(
-                  hintText: 'Your Email',
-                  prefixIcon: Icon(IconlyLight.message),
-                ),
-                controller: _emailController,
-              ),
-              const SizedBox(height: 30.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: TextIconButton(
-                  text: 'RESET PASSWORD',
-                  icon: IconlyLight.arrowRight,
-                  onClick: _handleResetPassword,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+          ScreenBackButton(
+            color: Colors.black,
+            onClick: () => _handleBack(context),
+          ),
+        ],
       ),
     );
   }
