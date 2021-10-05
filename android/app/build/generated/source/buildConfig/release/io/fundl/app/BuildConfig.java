7e4ff8c84f01f87e7e62684c8f28aa9f7a6111f1
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2021-10-05 22:43:19 +0200

    Disabled deep links

--- android/app/build/generated/source/buildConfig/release/io/fundl/app/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/io/fundl/app/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "io.fundl.app";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 5;
-  public static final String VERSION_NAME = "1.0.1";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "1.0.3";
 }
